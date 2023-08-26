2628b4e6e2e92a0c7bde813d6285d89f367b6c19
LuizGustavo <lgfs2903@gmail.com>
2023-08-26 17:57:37 -0300

    autor required

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, :body, :data, :img_url, presence: true
+  validates :title, :body, :data, :autor, :img_url, presence: true
   has_one_attached :img_url
 end
